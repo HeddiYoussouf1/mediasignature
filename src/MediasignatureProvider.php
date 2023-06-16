3dd6a91a3c55c1eda3b50c79932590cbb9783a29
HeddiYoussouf1 <heddiyoussouf@gmail.com>
2023-06-16 20:28:03 +0200

    initial commit

--- src/MediasignatureProvider.php
+++ src/MediasignatureProvider.php
@@ -5,7 +5,7 @@ class MediasignatureProvider extends ServiceProvider{
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/courier.php' => config_path('courier.php'),
+            __DIR__.'/../config/mediasignature.php' => config_path('mediasignature.php'),
         ]);
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
